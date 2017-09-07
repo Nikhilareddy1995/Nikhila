84ef226e7b6f3b6ce758e994d298b03edffdf2d5
Nikhilareddy1995 <nikhila.mits@gmail.com>
2017-09-07 11:05:27 +0530

    Add files via upload

---
+++ GitExample.java
@@ -0,0 +1,16 @@
+
+public class GitExample {
+	public void callMethod() {
+		for (int i = 1; i <= 5; i++) {
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println("HI");
+		GitExample g=new GitExample();
+		g.callMethod();
+		
+
+	}
+}
